3700e15dfab427b37b23d349f71cf200ec81b14b
Yahor Viarenich <gluks0575@gmail.com>
2019-05-14 21:15:06 +0200

    add get method list of car

---
+++ src/main/java/com/example/CarRental/service/CarService.java
@@ -0,0 +1,55 @@
+package com.example.CarRental.service;
+
+import com.example.CarRental.domain.model.CarEntity;
+import com.example.CarRental.domain.repository.CarRepository;
+import com.example.CarRental.model.AvailableCarsQuery;
+import com.example.CarRental.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarService {
+
+
+    @Autowired
+    CarRepository carRepository;
+
+
+
+    public List<Car> getAvailableCarsByParameter(AvailableCarsQuery query){
+        List<CarEntity> carEntities = carRepository
+                .getAvailableCarsByParameter(query.getAmount(),query.getCarBodyType(),
+                        query.getModel(),query.getReleaseYear());
+
+        return carEntities.stream()
+                .map(x->map(x))
+                .collect(Collectors.toList());
+    }
+
+
+    public Car map(CarEntity source){
+        return Car
+                .builder()
+                .id(source.getId())
+                .carBodyType(source.getCarBodyType())
+                .carStatus(source.getCarStatus())
+                .amount(source.getAmount())
+                .model(source.getModel())
+                .releaseYear(source.getReleaseYear())
+                .build();
+    }
+
+    public CarEntity map(Car source){
+        return CarEntity
+                .builder()
+                .carBodyType(source.getCarBodyType())
+                .carStatus(source.getCarStatus())
+                .amount(source.getAmount())
+                .model(source.getModel())
+                .releaseYear(source.getReleaseYear())
+                .build();
+    }
+}
