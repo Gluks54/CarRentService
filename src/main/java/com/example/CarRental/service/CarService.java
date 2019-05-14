a44d026f68e5c66106ae1669c4c7c80e77305dca
Yahor Viarenich <gluks0575@gmail.com>
2019-05-14 22:00:56 +0200

    fix property

--- src/main/java/com/example/CarRental/service/CarService.java
+++ src/main/java/com/example/CarRental/service/CarService.java
@@ -1,13 +1,17 @@
 package com.example.CarRental.service;
 
 import com.example.CarRental.domain.model.CarEntity;
+import com.example.CarRental.domain.model.ClientEntity;
 import com.example.CarRental.domain.repository.CarRepository;
+import com.example.CarRental.domain.repository.ClientRepository;
 import com.example.CarRental.model.AvailableCarsQuery;
 import com.example.CarRental.model.Car;
+import com.example.CarRental.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,6 +21,8 @@ public class CarService {
     @Autowired
     CarRepository carRepository;
 
+    @Autowired
+    ClientRepository clientRepository;
 
 
     public List<Car> getAvailableCarsByParameter(AvailableCarsQuery query){
